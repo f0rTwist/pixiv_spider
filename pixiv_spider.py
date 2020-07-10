5795355183b68036a4431eb34a78e8a82681d189
Twist Lau <R@F0rTwist.cn>
2020-07-10 12:22:48 +0800

    优化了数据持久化时机，添加修改时间功能

--- pixiv_spider.py
+++ pixiv_spider.py
@@ -9,9 +9,12 @@ import threading
 
 class Pixiv:
 
-    def __init__(self, moder, pn):
-        self.pixiv_username = '1466138257@qq.com'
-        self.pixiv_password = 'TwistedFate5'
+    def __init__(self, moder, pn, date=''):
+        self.pixiv_username = 'username'
+        self.pixiv_password = 'passwd'
+        if date != '':
+            date = '&date=' + date
+        self.date = date
         self.m_set = set()
         self.accounts_url = 'https://accounts.pixiv.net/login?return_to=https%3A%2F%2Fwww.pixiv.net%2F&lang=zh&source=pc&view_type=page'  # 登录界面连接
         self.login_url = 'https://accounts.pixiv.net/api/login?lang=zh'  # 登录URL
@@ -25,7 +28,7 @@ class Pixiv:
         }
         self.list_id = []
         self.list_url = []  # 排行榜url列表
-        self.url_rank = 'https://www.pixiv.net/ranking.php?'  # 排行榜url
+        self.url_rank = 'https://www.pixiv.net/ranking.php?'+date  # 排行榜url
         mode_ = 'daily' if moder == 1 else 'weekly' if moder == 2 else 'monthly'
         self.params_rank = {  # 排行榜url接口
             'mode': mode_,
@@ -74,25 +77,32 @@ class Pixiv:
     def id_get(self):  # 获取rank作品id
         self.init_set()
         if self.params_rank['mode'] == 'daily':
-            self.headers['referer'] = 'https://www.pixiv.net/ranking.php?mode=daily'
+            self.headers['referer'] = 'https://www.pixiv.net/ranking.php?mode=daily' + self.date
         elif self.params_rank['mode'] == 'weekly':
-            self.headers['referer'] = 'https://www.pixiv.net/ranking.php?mode=weekly'
+            self.headers['referer'] = 'https://www.pixiv.net/ranking.php?mode=weekly' + self.date
         elif self.params_rank['mode'] == 'monthly':
-            self.headers['referer'] = 'https://www.pixiv.net/ranking.php?mode=monthly'
+            self.headers['referer'] = 'https://www.pixiv.net/ranking.php?mode=monthly' + self.date
 
+        if self.date != '':
+            self.params_rank['date'] = self.date.lstrip('&date=')
         for u in range(int(self.params_rank['p'])):
             self.params_rank['p'] = str(u + 1)
-            a = requests.get(self.url_rank, headers=self.headers, params=self.params_rank)
+            try:
+                a = requests.get(self.url_rank, headers=self.headers, params=self.params_rank)
+            except requests.exceptions.SSLError:
+                print('\033[95mMax retries exceeded')
+                exit(0)
             url_get = a.text
             url_json = json.loads(url_get)
 
             for dict1 in url_json['contents']:  # 获取图片id
                 if dict1['illust_id'] not in self.m_set:
                     self.list_id.append(dict1['illust_id'])
-                    self.m_set.add(dict1['illust_id'])
+                    #self.m_set.add(dict1['illust_id'])
             # Pixiv.test(self)
 
     def url_get(self):  # 多线程获取url
+        self.date = self.date.lstrip('&date=')
         while True:
             self.glock.acquire()  # 加锁
             if len(self.list_id) == 0:
@@ -113,19 +123,6 @@ class Pixiv:
                     print ('\033[95m请求好多,好频繁,那里(指p站服务器)受不了了...(换个ip吧)')
                     exit(0)
 
-    # def test(self):   # 测试保存
-    #     j = 1
-    #     for i in self.dict_rank:
-    #         self.headers['referer'] = 'https://www.pixiv.net/ranking.php?mode=monthly'
-    #         pixiv_test = requests.get(i, headers=self.headers)
-    #         path = self.params_rank['mode'] + '/' + str(j) + '.jpg'
-    #         if not os.path.exists(self.params_rank['mode']):
-    #             os.mkdir(self.params_rank['mode'])
-    #         with open(path, 'wb') as f:
-    #             f.write(pixiv_test.content)
-    #             j += 1
-    #             print(f'图片{i}正在保存...')
-
     def download(self):  # 多线程下载
         while True:
             self.glock.acquire()  # 加锁
@@ -134,33 +131,38 @@ class Pixiv:
                 break
             else:
                 if not os.path.exists(
-                        str(self.params_rank['mode']) + '-' + time.strftime("%Y-%m-%d", time.localtime())):
-                    os.mkdir(str(self.params_rank['mode']) + '-' + time.strftime("%Y-%m-%d", time.localtime()))
+                        str(self.params_rank['mode']) + '-' + self.date if self.date != '' else time.strftime("%Y%m%d", time.localtime())):
+                    os.mkdir(str(self.params_rank['mode']) + '-' + self.date if self.date != '' else time.strftime("%Y%m%d", time.localtime()))
                 url = self.list_url.pop(0)  # 提取列表第一个URL并删除
                 self.glock.release()  # 释放锁
                 # 修改文件名
-                if url[-3:] == 'jpg':
-                    path = str(self.params_rank['mode']) + '-' + time.strftime("%Y-%m-%d",
-                                                                               time.localtime()) + '/' + str(
-                        self.i) + '.jpg'
-                elif url[-3:] == 'png':
-                    path = str(self.params_rank['mode']) + '-' + time.strftime("%Y-%m-%d",
-                                                                               time.localtime()) + '/' + str(
-                        self.i) + '.png'
-
-                pixiv_img = requests.get(url, headers=self.headers)
-                with open(path, 'wb') as f:
+                sid = str(url).split('/')[-1]
+                path_ = (str(self.params_rank['mode']) + '-' + (self.date if self.date != '' else
+                         time.strftime("%Y%m%d", time.localtime())) + '/' + sid)
+                try:
+                    pixiv_img = requests.get(url, headers=self.headers)
+                except:
+                    self.dump_set()
+                    print ('\033[95m请求好多,好频繁,那里(指p站服务器)受不了了...(图下载到一半,待会儿接着来吧)')
+                    exit(0)
+                with open(path_, 'wb') as f:
                     f.write(pixiv_img.content)
-                    print('图片' + str(self.i) + '正在保存...' + path)
+                    print('图片' + path_ + '正在保存...')
                     self.i += 1
+
+                self.m_set.add(int(sid.split('_')[0]))
 
 
 def main():
-    moder = input('请选择排行榜的类型（日:1/周:2/月:3）:')
+    moder = input('请选择排行榜的类型(日:1/周:2/月:3):')
+    try:
+        date = input('输入日期[可选,格式类似20200101]:')
+    except:
+        date = ''
+        pass
     pn = input('请输入你想要多少页(50/页):')
 
-    pixivc = Pixiv(moder, pn)
-    # pixivc.login()
+    pixivc = Pixiv(moder, pn, date=str(date))
     pixivc.id_get()
 
     print('获取链接中...')
@@ -168,7 +170,7 @@ def main():
         urlget = threading.Thread(target=pixivc.url_get())
         urlget.start()
 
-    print('图片保存到' + pixivc.params_rank['mode'] + '-' + time.strftime("%Y-%m-%d", time.localtime()))
+    print('图片保存到' + pixivc.params_rank['mode'] + '-' + str(date) if str(date)!= '' else time.strftime("%Y%m%d", time.localtime()))
     for i in range(3):
         download = threading.Thread(target=pixivc.download())
         download.start()
